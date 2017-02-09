ca024b58eb26d0f300c5bc93b94b4eb5e4a08dab
shilejian <cdshilejian@jd.com>
2017-02-09 11:13:03 +0800

    1

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -28,7 +28,10 @@
     </div>
     <div v-show="detailShow" class="detail">
       <div class="detail-wrapper clearfix">
-        <div class="detail-main"></div>
+        <div class="detail-main">
+         <h1 class="name">{{seller.name}}</h1>
+          <star :size="48" :score="seller.score"></star>
+        </div>
       </div>
       <div class="detail-close">
         <i class="icon-close"></i>
@@ -38,6 +41,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import star from 'components/star/star';
+
   export default{
       props: {
           seller: {
@@ -56,6 +61,9 @@
       },
       created() {
         this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      },
+      components: {
+          star
       }
   };
 </script>
@@ -177,16 +185,22 @@
       overflow: hidden
       background: rgba(7, 17, 27, 0.8)
       .detail-wrapper
+          width: 100%
           min-height: 100%
           .detail-main
               margin-top: 64px
               padding-bottom: 64px
-          .detail-close
-              position: relative
-              width: 32px
-              height: 32px
-              margin: -64px auto 0 auto
-              clear: both
-              font-size: 32px
+              .name
+                  line-height: 16px
+                  text-align: center
+                  font-size: 16px
+                  font-weight: 700
+      .detail-close
+          position: relative
+          width: 32px
+          height: 32px
+          margin: -64px auto 0 auto
+          clear: both
+          font-size: 32px
 
 </style>
